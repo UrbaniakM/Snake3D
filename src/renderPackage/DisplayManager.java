8c24d32b9e158899a6c8f6512fa79f6e9fcc42f2
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2017-09-27 01:09:54 +0200

    Created renderPackage

---
+++ src/renderPackage/DisplayManager.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package renderPackage;
+
+import org.lwjgl.opengl.*;
+import org.lwjgl.LWJGLException;
+
+
+/**
+ *
+ * @author Mateusz
+ */
+public class DisplayManager {
+    
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+    private static final int FPS_CAP = 120;
+    
+    public static void createDisplay(){
+        
+        ContextAttribs attribs = new ContextAttribs(3,2).withForwardCompatible(true).withProfileCore(true);
+        
+        try{
+            Display.setDisplayMode(new DisplayMode(WIDTH,HEIGHT));
+            Display.create(new PixelFormat(), attribs);
+            Display.setTitle("Mateusz Urbaniak - SNAKE 3D");
+        } catch (LWJGLException e){
+            e.printStackTrace();
+        }
+        
+        GL11.glViewport(0,0,WIDTH,HEIGHT);
+    }
+    
+    public static void updateDisplay(){
+        Display.sync(FPS_CAP);
+        Display.update();
+    }
+    
+    public static void closeDisplay(){
+        Display.destroy();
+    }
+}
---
+++ src/snake/SNAKE.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snake;
+
+import renderPackage.DisplayManager;
+import org.lwjgl.opengl.*;
+
+/**
+ *
+ * @author Mateusz
+ */
+public class SNAKE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DisplayManager.createDisplay();
+        while(!Display.isCloseRequested()) {
+           // DisplayManager.updateDisplay();
+        }
+        DisplayManager.closeDisplay();
+    }
+    
+}

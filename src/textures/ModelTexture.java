48811b6f3fd6fae1915409d7465e766797de62a9
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2017-09-27 02:43:56 +0200

    Added textures package.

---
+++ src/textures/ModelTexture.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textures;
+
+/**
+ *
+ * @author Mateusz
+ */
+public class ModelTexture {
+    private int textureID;
+    
+    public ModelTexture(int textureID) {
+        this.textureID = textureID;
+    }
+    
+    public int getTextureID() {
+        return this.textureID;
+    }
+}

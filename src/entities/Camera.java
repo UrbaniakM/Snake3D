4247f29c923e472c79820cbb43b930c3c55880d9
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2017-09-27 04:42:45 +0200

    Added Model, View and Projection Matrices.

---
+++ src/entities/Camera.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.util.vector.Vector3f;
+
+/**
+ *
+ * @author Mateusz
+ */
+public class Camera {
+    private Vector3f position = new Vector3f(0,0,0);
+    private float pitch;
+    private float yaw;
+    private float roll;
+    
+    public Camera(){
+        
+    }
+
+    public void move() {
+        if(Keyboard.isKeyDown(Keyboard.KEY_W)){
+            position.z -= 0.02f;
+        }
+        if(Keyboard.isKeyDown(Keyboard.KEY_S)){
+            position.z += 0.02f;
+        }
+        if(Keyboard.isKeyDown(Keyboard.KEY_A)){
+            position.x -= 0.02f;
+        }
+        if(Keyboard.isKeyDown(Keyboard.KEY_D)){
+           position.x += 0.02f;
+        }
+    }
+    
+    public Vector3f getPosition() {
+        return position;
+    }
+
+    public float getPitch() {
+        return pitch;
+    }
+
+    public float getYaw() {
+        return yaw;
+    }
+
+    public float getRoll() {
+        return roll;
+    }
+    
+    
+}
--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -5,7 +5,9 @@
  */
 package shaders;
 
+import entities.Camera;
 import org.lwjgl.util.vector.Matrix4f;
+import toolbox.Maths;
 
 /**
  *
@@ -16,6 +18,8 @@ public class StaticShader extends ShaderProgram{
     private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
     
     private int location_transformationMatrix;
+    private int location_projectionMatrix;
+    private int location_viewMatrix;
     
     public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -30,9 +34,20 @@ public class StaticShader extends ShaderProgram{
     @Override
     protected void getAllUniformLocation() {
         location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+        location_projectionMatrix = super.getUniformLocation("projectionMatrix");
+        location_viewMatrix = super.getUniformLocation("viewMatrix");
     }
     
     public void loadTransformationMatrix(Matrix4f matrix) {
         super.loadMatrix(location_transformationMatrix,matrix);
+    }
+    
+    public void loadProjectionMatrix(Matrix4f projection) {
+        super.loadMatrix(location_projectionMatrix,projection);
+    }
+    
+    public void loadViewMatrix(Camera camera) {
+        Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+        super.loadMatrix(location_viewMatrix,viewMatrix);
     }
 }

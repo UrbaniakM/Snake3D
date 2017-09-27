c68f377130a7d96616cf41e8b6356eb0d63a8b77
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2017-09-27 02:25:36 +0200

    Changer ObjLoader and Renderer classes.

--- src/renderPackage/ObjLoader.java
+++ src/renderPackage/ObjLoader.java
@@ -6,7 +6,7 @@
 package renderPackage;
 
 import org.lwjgl.opengl.*;
-import java.nio.FloatBuffer;
+import java.nio.*;
 import org.lwjgl.BufferUtils;
 import java.util.*;
 
@@ -19,14 +19,28 @@ public class ObjLoader {
     private List<Integer> vaos = new ArrayList <> ();
     private List<Integer> vbos = new ArrayList <> ();
     
-    public RawModel loadToVAO(float [] positions) {
+    public RawModel loadToVAO(float [] positions, int[] indices) {
         int vaoID = createVAO();
+        bindIndicesBuffer(indices);
         storeDataInAttributeList(0,positions);
         unbindVAO();
-        return new RawModel(vaoID, positions.length/3);
+        return new RawModel(vaoID, indices.length);
     }
     
+    private void bindIndicesBuffer(int[] indices) {
+        int vboID = GL15.glGenBuffers();
+        vbos.add(vboID);
+        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboID);
+        IntBuffer buffer = storeDataInIntBuffer(indices);
+        GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+    }
     
+    private IntBuffer storeDataInIntBuffer(int[] data) {
+        IntBuffer buffer = BufferUtils.createIntBuffer(data.length);
+        buffer.put(data);
+        buffer.flip();
+        return buffer;
+    }
     
     public void cleanUp() {
         for(int vbo:vbos) {

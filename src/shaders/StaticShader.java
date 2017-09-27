d21edb9c29d88bfa3a113bff19c4bef1236339a2
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2017-09-27 06:06:03 +0200

    Created Light class and updated shaders.

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -6,6 +6,7 @@
 package shaders;
 
 import entities.Camera;
+import entities.Light;
 import org.lwjgl.util.vector.Matrix4f;
 import toolbox.Maths;
 
@@ -20,6 +21,8 @@ public class StaticShader extends ShaderProgram{
     private int location_transformationMatrix;
     private int location_projectionMatrix;
     private int location_viewMatrix;
+    private int location_lightPosition;
+    private int location_lightColour;
     
     public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -36,6 +39,8 @@ public class StaticShader extends ShaderProgram{
         location_transformationMatrix = super.getUniformLocation("transformationMatrix");
         location_projectionMatrix = super.getUniformLocation("projectionMatrix");
         location_viewMatrix = super.getUniformLocation("viewMatrix");
+        location_lightPosition = super.getUniformLocation("lightPosition");
+        location_lightColour = super.getUniformLocation("lightColour");
     }
     
     public void loadTransformationMatrix(Matrix4f matrix) {
@@ -49,5 +54,10 @@ public class StaticShader extends ShaderProgram{
     public void loadViewMatrix(Camera camera) {
         Matrix4f viewMatrix = Maths.createViewMatrix(camera);
         super.loadMatrix(location_viewMatrix,viewMatrix);
+    }
+    
+    public void loadLight(Light light) {
+        super.loadVector(location_lightPosition,light.getPosition());
+        super.loadVector(location_lightColour,light.getColour());
     }
 }
